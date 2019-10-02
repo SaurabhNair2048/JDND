fb07319264d71910c745a9b9059caf82cbf4ee4f
SaurabhNair2048 <sbhn4evr@protonmail.com>
2019-10-02 21:58:25 +0530

    Updated Request classes

--- projects/P04-eCommerce Application/starter_code/src/main/java/com/example/demo/model/requests/CreateUserRequest.java
+++ projects/P04-eCommerce Application/starter_code/src/main/java/com/example/demo/model/requests/CreateUserRequest.java
@@ -6,6 +6,30 @@ public class CreateUserRequest {
 
 	@JsonProperty
 	private String username;
+	/*
+	 * Added password and confirm password to perform JWT authentication
+	 */
+	@JsonProperty
+	private String password;
+	
+	@JsonProperty
+	private String confirmPassword;
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
+	}
 
 	public String getUsername() {
 		return username;
